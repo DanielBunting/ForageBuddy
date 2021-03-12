45dfe6c8cc5cdf3dc5c84e48396dec1001438e12
Danie Bunting <daniel.bunting93@gmail.com>
2021-03-12 20:34:41 +0000

    Adds the initial unit test project.

---
+++ ForageBuddy.Tests/ImageParserTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace ForageBuddy.Tests
+{
+    public class ImageParserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
---
+++ ForageBuddy.Tests/PlayerScoreTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForageBuddy.Tests
+{
+    class PlayerScoreTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
