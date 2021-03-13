06fe42093eaeeffd13159258558848eb5f09509e
Danie Bunting <daniel.bunting93@gmail.com>
2021-03-13 17:10:29 +0000

    Adds initial Unit tests around the NameReader.

--- ForageBuddy.Tests/NameReaderTests.cs
+++ ForageBuddy.Tests/NameReaderTests.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using Moq;
 using Microsoft.Extensions.Logging;
+using System.Drawing;
+using System.Collections.Generic;
+using LockedBitmapUtil.Extensions;
 
 namespace ForageBuddy.Tests
 {
@@ -9,10 +12,45 @@ namespace ForageBuddy.Tests
         private NameReader _nameReader;
         private Mock<ILogger<NameReader>> _logger;
 
+        private List<(Bitmap image, string expectedName)> _testImages;
+
+        const int scalar = 8;
+        const int BrightnessThreshold = 100;
+
         [SetUp]
         public void Setup()
         {
+            _logger = new Mock<ILogger<NameReader>>();
+            _nameReader = new NameReader(_logger.Object);
 
+            _testImages = new List<(Bitmap image, string expectedName)>()
+            {
+                (Properties.Resources.NameReader_Annapurna_1, "Annapurna"),
+                (Properties.Resources.NameReader_Jaxxa_1, "Jaxxa"),
+                (Properties.Resources.NameReader_Raffaella_1, "Raffaella"),
+                //(Properties.Resources.NameReader_Theredwench_1, "Theredwench"), // TODO: Currently broken.
+                //(Properties.Resources.NameReader_Yeetmcskeet_1, "Yeetmcskeet")  // TODO: Currently broken
+            };
+        }
+
+
+        [Test]
+        public void ReadDutyReportName_WhenGivenImageOfName_ReturnsCorrectName()
+        {
+            foreach (var (image, expectedName) in _testImages)
+            {
+                var transformed = image
+                    .ToLockedBitmap()
+                    .Resize(image.Width * scalar, image.Height * scalar)
+                    .ToBinaryImage(BrightnessThreshold, Color.White, Color.Black)
+                    .ToBitmap();
+
+                // Act
+                var recievedName = _nameReader.ReadDutyReportName(transformed);
+
+                // Assert
+                Assert.AreEqual(expectedName, recievedName);
+            }
         }
     }
 }
--- ForageBuddy.Tests/Properties/Resources.Designer.cs
+++ ForageBuddy.Tests/Properties/Resources.Designer.cs
@@ -59,5 +59,55 @@ namespace ForageBuddy.Tests.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NameReader_Annapurna_1 {
+            get {
+                object obj = ResourceManager.GetObject("NameReader_Annapurna_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NameReader_Jaxxa_1 {
+            get {
+                object obj = ResourceManager.GetObject("NameReader_Jaxxa_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NameReader_Raffaella_1 {
+            get {
+                object obj = ResourceManager.GetObject("NameReader_Raffaella_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NameReader_Theredwench_1 {
+            get {
+                object obj = ResourceManager.GetObject("NameReader_Theredwench_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap NameReader_Yeetmcskeet_1 {
+            get {
+                object obj = ResourceManager.GetObject("NameReader_Yeetmcskeet_1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
