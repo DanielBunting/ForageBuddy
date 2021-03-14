7d2e4bb063b51dfa4463f94b8425a0d4fffb41e3
Danie Bunting <daniel.bunting93@gmail.com>
2021-03-14 22:42:56 +0000

    Adds ChestGrouping tests.

--- ForageBuddy.Tests/ChestGroupingTests.cs
+++ ForageBuddy.Tests/ChestGroupingTests.cs
@@ -25,35 +25,69 @@ namespace ForageBuddy.Tests
         [Test]
         public void OnConstruction_ImagePersisted_IsTheImageInTheObject()
         {
-            
+            Assert.AreEqual(_lockedBitmap, _chestGrouping.NameSector);
         }
 
         [Test]
-        public void AddChest_OnNewBoneBox_UpdatesBoneBoxCounts()
+        public void OnConstruction_LocationPersisted_IsTheInitialLocation()
         {
-            
+            Assert.AreEqual(_initialTopLeft, _chestGrouping.GroupLocation);
         }
 
-        [Test]
-        public void AddChest_OnNewFetishJar_UpdatesFetishJarCounts()
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void AddChest_OnNewBoneBox_UpdatesBoneBoxCounts(int chestsAdded)
         {
+            for(int x = 0; x < chestsAdded; x++)
+                _chestGrouping.AddChest(ChestType.BoneBox, _initialTopLeft);
 
+            Assert.AreEqual(chestsAdded + 1, _chestGrouping.BoneBox);
         }
 
-        [Test]
-        public void AddChest_OnNewCursedChest_UpdatesCusedChestCounts()
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void AddChest_OnNewFetishJar_UpdatesFetishJarCounts(int chestsAdded)
         {
+            for (int x = 0; x < chestsAdded; x++)
+                _chestGrouping.AddChest(ChestType.FetishJar, _initialTopLeft);
 
+            Assert.AreEqual(chestsAdded, _chestGrouping.FetishJar);
         }
 
-        [Test]
-        public void AccChest_OnNewLocationWhenFurtherUpOrLeft_UpdatesLocation()
-        { 
-        }
-
-        [Test]
-        public void AccChest_OnNewLocationWhenLowerOrRight_DoesNotUpdateLocation()
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void AddChest_OnNewCursedChest_UpdatesCusedChestCounts(int chestsAdded)
         {
+            for (int x = 0; x < chestsAdded; x++)
+                _chestGrouping.AddChest(ChestType.CursedChest, _initialTopLeft);
+
+            Assert.AreEqual(chestsAdded, _chestGrouping.CursedChest);
+        }
+
+        [Test]
+        public void AddChest_OnNewLocationWhenFurtherUpOrLeft_UpdatesLocation()
+        {
+            var point = new Point(50, 50);
+
+            _chestGrouping.AddChest(ChestType.BoneBox, point);
+
+            Assert.AreEqual(point, _chestGrouping.GroupLocation);
+        }
+
+        [Test]
+        public void AddChest_OnNewLocationWhenLowerOrRight_DoesNotUpdateLocation()
+        {
+            var point = new Point(600, 600);
+
+            _chestGrouping.AddChest(ChestType.BoneBox, point);
+
+            Assert.AreEqual(_initialTopLeft, _chestGrouping.GroupLocation);
         }
     }
 }
